d9db98a9fde71d12193ca99e3c26dcfed16c5ab4
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-06-01 15:41:41 -0500

    changed task count to nextID. taskcount canot work since delted tasks could cause an issue wjere the task count is an id already taken

--- todo/todo/taskList.cs
+++ todo/todo/taskList.cs
@@ -4,22 +4,32 @@ class TaskList
     List<Task> todo = new List<Task>();
     List<Task> inProgress = new List<Task>();
     List<Task> completed = new List<Task>();
+    private int NextID;
     
-    publid void Add(Task t)
+    publid void Add(string title, string priority)
     {
+        Task t = new Task(NextID, title, priority);
         switch(t.priority)
         {
             case "todo":
                 todo.Add(t);
+                NextID++;
                 break;
             case "inProgress":
                 inProgress.Add(t);
+                NextID++;
                 break;
             case "completed":
                 completed.Add(t);
+                NextID++;
                 break;
         }
         //add linq statement here to add to sqlite as well
+    }
+
+    private void DatabaseAdd(int id, string newTitle, string newPriority)
+    {
+        
     }
 
     public void Remove(Task t)
